857ea180f353cbd10f78f125eb92883bb32c0478
minkhant21 <kaungminkhant6889@gmail.com>
2023-02-09 13:13:34 +0630

    start

---
+++ src/views/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+        <blockquote>
+        <h1>Welcome</h1>
+
+        </blockquote>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+blockquote {
+  padding: 20px;
+  box-shadow: inset 0 -3em 3em rgba(0, 0, 0, 0.1), 0 0 0 2px rgb(255, 255, 255),
+    0.3em 0.3em 1em rgba(0, 0, 0, 0.3);
+    text-align: center;
+    margin-top: 50px;
+}
+</style>
