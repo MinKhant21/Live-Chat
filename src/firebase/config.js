f91e7b121151ee2acd73154abc3be109888d2994
minkhant21 <kaungminkhant6889@gmail.com>
2023-02-10 09:56:07 +0630

    firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import firestore from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCpViQHABU5N9buzDjAPU_5AkPviIgGbCQ",
+  authDomain: "vue-blog-system-d3ffa.firebaseapp.com",
+  projectId: "vue-blog-system-d3ffa",
+  storageBucket: "vue-blog-system-d3ffa.appspot.com",
+  messagingSenderId: "633431634387",
+  appId: "1:633431634387:web:abeabc456d73689417bf26"
+};
+
+let app = firebase.initializeApp(firebaseConfig)
+let db = app.firestore()
+
+export {db}
